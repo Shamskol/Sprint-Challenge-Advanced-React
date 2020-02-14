ceb17ddd6b07a1570d2b225b2d14d34d550052a0
Kolade-Junaid <koladejun@yahoo.com>
2020-02-14 10:40:00 +0000

    Fetched data from the API

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import axios from "axios";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+     data: [],
+
+    };
+  }
+  
+
+
+  componentDidMount() {
+ debugger
+
+axios.get ('http://localhost:5000/api/players')
+
+      .then(response => {
+        this.setState({data: response.data});
+        debugger
+// console.log(this.state.data);
+// console.log(response.data)
+     })
+     .catch(error => { 
+
+      console.log(error);
+     
+      });
+    }
+
+  render() {
+
+    return (
+      <div className="App">
+        Hello
+      </div>
+    );
+    }
+  
 }
+  
+
+
+
+
+
+
 
 export default App;
+
+
+
+
+
+
+
+ 
