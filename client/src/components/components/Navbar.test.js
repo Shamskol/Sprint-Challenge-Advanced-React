2b7ba5e4b76bea2e705c29d47ab403b726d9840b
Kolade-Junaid <koladejun@yahoo.com>
2020-03-13 17:36:00 +0000

    CWrote some tests in Navbar.test and App.test.js files

---
+++ client/src/components/components/Navbar.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {render} from '@testing-library/react';
+import Navbar from "./Navbar";
+
+test ("renders Navbar header", () => {
+//Arrange
+const {getByText} = render(<Navbar/>);
+//Act
+const header = getByText(/women's world cup/i);
+//Assert
+expect(header)
+expect(header).toBeTruthy();
+expect(header).not.toBeFalsy();
+
+});
+
+test ("" , () => {
+
+
+});
+
+test ("" , () => {
+
+
+});
+
+test ("" , () => {
+
+
+});
+
+test ("" , () => {
+
+
+ });
